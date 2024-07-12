3591817220a498f4428a16745136c30bd68ca1da
SebitaBravo <sebastianalejandrobravocampos@gmail.com>
2024-07-12 19:05:22 -0400

    update mysql dependency to use mysql2/promise pool connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,19 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'gestor_trabajadores',
   port: 8889,
-});
-
-connection.connect((err) => {
-    if(err) {
-        console.error('Error conexion con la db', err.stack);
-        return;
-    }
-    console.log('Conexion con db');
 });
 
 module.exports = connection;
